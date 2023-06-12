6c09b55303b9b4b3335a2be3865c41f503b6fa91
Kyle Li <112967756+s-kyli@users.noreply.github.com>
2023-06-11 19:00:54 -0700

    Create ClientLaserScript.lua

---
+++ Client/ClientLaserScript.lua
@@ -0,0 +1,26 @@
+local RL = game:GetService("ReplicatedStorage")
+local CL = RL:WaitForChild("ClientLaser")
+
+-- creates a new laser beam on client side that has no functionality, but is just for the looks and makes the laser beam move smoother since this is a localscript and it is being run on client script.
+
+CL.OnClientEvent:Connect(function(laser:Part)
+
+	local newLaser = laser:Clone()
+	newLaser.Parent = workspace
+	laser:Destroy()
+
+	newLaser.Touched:Connect(function(hit)
+
+		if hit.CollisionGroup == "Obstacles" then
+			newLaser:Destroy()
+		end
+
+		wait(3)
+		--destroy laserbeam if not destroyed already
+		if newLaser then
+			newLaser:Destroy()
+		end
+
+	end)
+
+end)
